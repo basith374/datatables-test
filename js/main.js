dfb45a3820aa06a1bbafe7c36c23994d0808a590
Basith Kunimal <basith374@gmail.com>
2016-09-01 22:36:32 +0530

    initial

---
+++ js/main.js
@@ -0,0 +1,190 @@
+$("#table")
+	.DataTable({
+        // dom: 'Bfrtip',
+		buttons: [
+            {
+                extend: 'print',
+                customize: function(win) {
+                    $(win.document.body)
+                        .css('font-size', '10pt');
+                    $(win.document.body)
+                        .find('table')
+                        .addClass('compact')
+                        .css('font-size', 'inherit');
+                },
+                footer: true
+            }
+		],
+		ajax: {
+			url: 'data.json',
+			dataSrc: 'items'
+		},
+		columns: [
+			{data: 'id'},
+			{data: 'name'},
+			{data: 'company'},
+			{data: 'email'},
+			{data: 'phone'},
+			{data: 'balance'}
+		],
+		footerCallback: function ( row, data, start, end, display ) {
+            var api = this.api(), data;
+ 
+            // Remove the formatting to get integer data for summation
+            var intVal = function ( i ) {
+                return typeof i === 'string' ?
+                    i.replace(/[\$,]/g, '')*1 :
+                    typeof i === 'number' ?
+                        i : 0;
+            };
+ 
+            // Total over all pages
+            total = api
+                .column( 5, { search: 'applied'} )
+                .data()
+                .reduce( function (a, b) {
+                    return intVal(a) + intVal(b);
+                }, 0 );
+ 
+            // Total over this page
+            pageTotal = api
+                .column( 5, { page: 'current'} )
+                .data()
+                .reduce( function (a, b) {
+                    return intVal(a) + intVal(b);
+                }, 0 );
+ 
+            // Update footer
+            $( api.column( 5 ).footer() ).html(
+                '$'+pageTotal +' ( $'+ total +' total )'
+            );
+        }
+	});
+
+function escapeRegExp(string){
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
+}
+
+new Vue({
+    data: {
+        rows: [],
+        cached: [],
+        curPage: 1,
+        limit: 10
+    },
+    computed: {
+        totalPages: function() {
+            return Math.ceil(this.rows.length / this.limit);
+        },
+        curPageItems: function() {
+            var startIndex = this.curPage / this.limit;
+            console.log('redrawing ' + this.rows.length )
+            return this.rows.slice(startIndex, startIndex + this.limit);
+        },
+        pages: function() {
+            var limit = this.limit;
+            var rows = this.rows;
+            var curPage = this.curPage;
+            var length = rows.length;
+            var totalPages = this.totalPages;
+            if(totalPages > 7) {
+                var pages = []; // paginated pages
+
+                // first page
+                pages.push({index:1});
+                // front cover
+                if(curPage < 5) {
+                    pages.push({index:2});
+                } else {
+                    pages.push({index:'...'});
+                }
+                // middle "3"
+                var middle_start = curPage - 1;
+                var middle_end = curPage + 2;
+                if(curPage < 5) {
+                    middle_start = 3;
+                    middle_end = 6;
+                } else if(curPage > totalPages - 3) {
+                    middle_start = totalPages - 4;
+                    middle_end = totalPages - 1;
+                }
+                for(var i = middle_start; i < middle_end; i++) {
+                    pages.push({index: i});
+                }
+                // back cover
+                if(curPage < totalPages - 3) {
+                    pages.push({index:'...'});
+                } else {
+                    var back_cover = totalPages - 1;
+                    pages.push({index:back_cover});
+                }
+                // last page
+                pages.push({index:totalPages});
+
+                return pages;
+            } else {
+                var pages = [];
+                for(var i = 1; i <= totalPages; i++) {
+                    pages.push({index: i});
+                }
+                return pages;
+            }
+        }
+    },
+    el: 'body',
+    ready: function() {
+        var self = this;
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/data.json');
+        xhr.onload = function() {
+            if(xhr.status == 200) {
+                var data = JSON.parse(xhr.responseText);
+                // console.log(data);
+                self.rows = data.items;
+            }
+        }
+        xhr.send();
+    },
+    methods: {
+        change: function(e) {
+            this.curPage = 1;
+            var search = e.target.value;
+            if(search) {
+                if(!this.cached.length) {
+                    console.log('caching');
+                    this.cached = this.rows;
+                }
+                var regex = new RegExp('.*' + escapeRegExp(search) + '.*', 'i');
+                this.rows = this.cached.filter(function(row) {
+                    for(key in row) {
+                        if(regex.test(row[key])) {
+                            return true
+                        }
+                    }
+                    return false;
+                });
+            } else {
+                console.log('restoring cache');
+                this.rows = this.cached;
+                this.cached = [];
+            }
+        },
+        changePage: function(page, e) {
+            e.preventDefault();
+            if(Number.isInteger(page)) {
+                this.curPage = page;
+            }
+        },
+        next: function(e) {
+            e.preventDefault();
+            if(this.curPage < this.totalPages)
+            this.curPage++;
+        },
+        previous: function(e) {
+            e.preventDefault();
+            if(this.curPage > 1) {
+                this.curPage--;
+            }
+        }
+    }
+});
